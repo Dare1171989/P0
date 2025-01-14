58c60c41e124e51f81962868350bf27bbe4356b4
Darren Newland <darrennewland@Darrens-MBP.ad.pcc.edu>
2025-01-14 11:06:27 -0800

    adds P5 files

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let xLoc = 50;
 let xSpeed = 1;
+let yLoc = 50;
+let ySpeed = 50;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -10,7 +13,12 @@ function draw() {
   if(xLoc > width - 50|| xLoc < 0){
     xLoc = xLoc * -1;
   }
+  yLoc = yLoc + ySpeed;
+  if(yLoc> height - 50|| yLoc <0){
+    yLoc - yLoc * -1;
+  }
   
+
 
   background(0, 0, 220);
   fill(255, 0, 255);
